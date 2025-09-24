2ff210440db958b34618eb8b3547984ef0b3f723
Tomasz1306 <bogdantomek987@gmail.com>
2025-09-24 16:37:48 +0200

    ref: refactored BFS, using predicate to not repeat code

--- zadanie/src/main/java/service/impl/FileCabinet.java
+++ zadanie/src/main/java/service/impl/FileCabinet.java
@@ -9,6 +9,8 @@ import service.Cabinet;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class FileCabinet implements Cabinet {
 
@@ -21,97 +23,62 @@ public class FileCabinet implements Cabinet {
 
     public FileCabinet() {
 
-    this.generateData(15000,3000,200);
+    this.generateData(15000,300,200);
 //    for (Folder folder : folders) {
 //        System.out.println(folder);
 //    }
+        String name = "Random 10000";
 
-        Instant start1 = Instant.now();
-        Optional<Folder> folderA = findFolderByName("Random 10000 59");
-        Instant end1 = Instant.now();
-        Instant start3 = Instant.now();
-        Optional<Folder> folderC = findFolderByName3("Random 10000 59");
-        Instant end3 = Instant.now();
-        System.out.println("CZAS REKURENCJI: " + Duration.between(start1, end1).toString());
-        System.out.println("BFS: " + Duration.between(start3, end3).toString());
-        System.out.println(folderA.map(folder -> "Znaleziono: " + folder.getName()).orElse("Nie znaleziono folderu A"));
-        System.out.println(folderC.map(folder -> "ZNALEZIONO: " + folder.getName()).orElse("Nie znaleziono folderu C"));
-//        findFoldersBySize("LARGE");
-//        System.out.println("Number of folders: " + count());
+        Instant start = Instant.now();
+        Optional<Folder> findedFolder = findFolderByName(name);
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+        System.out.println(findedFolder.isPresent() ? "Znaleziono: " + findedFolder.get().getName() : "Nie znaleziono: " + name);
+        System.out.println("CZAS: " + duration.toSeconds() + "s " + duration.toMillis() + "ms");
+
     }
 
     @Override
     public Optional<Folder> findFolderByName(String name) {
-        for (Folder folder: folders) {
-            Folder findedFolder = searchFolderWithName(folder, name);
-            if (findedFolder != null) {
-                return Optional.of(findedFolder);
-            }
-        }
-        return Optional.empty();
+        Predicate<Folder> predicate = s -> s.getName().equals(name);
+        return findOneWithBFS(predicate);
     }
-
-    public Optional<Folder> findFolderByName3(String name) {
-        Folder folder = BFS(name);
-        if (folder != null) {
-            return Optional.of(folder);
-        }
-        return Optional.empty();
-    }
-
-    private Folder BFS(String name) {
-        Iterator<Folder> mainIterator = this.folders.iterator();
-        Folder source = mainIterator.next();
-
-        Set<String> visited = new HashSet<>();
-
-        ArrayDeque<Folder> queue = new ArrayDeque<>();
-
-        visited.add(source.getName());
-        queue.add(source);
-
-        while(!queue.isEmpty()) {
-            source = queue.poll();
-            if (source.getClass() == DefaultFolder.class) {
-                if (source.getName().equals(name)) {
-                    return source;
-                }
-            }
-
-            if (source.getClass() == DefaultMultiFolder.class) {
-                if (source.getName().equals(name)) {
-                    return source;
-                }
-
-                for (Folder n : ((DefaultMultiFolder) source).getFolders()) {
-                    if (!visited.contains(n.getName())) {
-                        visited.add(n.getName());
-                        queue.add(n);
-                    }
-                }
-            }
-            if (mainIterator.hasNext()) {
-                Folder n = mainIterator.next();
-                if (!visited.contains(n.getName())) {
-                    visited.add(n.getName());
-                    queue.add(n);
-                }
-            }
-
-        }
-        return null;
-    }
-
 
     @Override
     public List<Folder> findFoldersBySize(String size) {
-        List<Folder> foldersWithSize = new ArrayList<>();
-        for (Folder folder: folders) {
-            searchFolderWithSize(folder, size, foldersWithSize);
-        }
-        System.out.println(foldersWithSize);
-        return foldersWithSize;
+        Predicate<Folder> predicate = s -> s.getSize().equals(size);
+        return findAllWithBFS(predicate);
     }
+
+    private Optional<Folder> findOneWithBFS(Predicate<Folder> predicate) {
+        return getFoldersWithBFS().filter(predicate).findFirst();
+    }
+
+    private List<Folder> findAllWithBFS(Predicate<Folder> predicate) {
+        return getFoldersWithBFS().filter(predicate).toList();
+    }
+
+    private Stream<Folder> getFoldersWithBFS() {
+        Deque<Folder> deque = new ArrayDeque<>(this.folders);
+        List<Folder> collectedFolders = new LinkedList<>();
+        while(!deque.isEmpty()) {
+            Folder folder = deque.removeFirst();
+            collectedFolders.add(folder);
+            if (folder instanceof DefaultMultiFolder) {
+                ((DefaultMultiFolder) folder).getFolders().forEach(deque::push);
+            }
+        }
+        return collectedFolders.stream();
+    }
+
+//    @Override
+//    public List<Folder> findFoldersBySize(String size) {
+//        List<Folder> foldersWithSize = new ArrayList<>();
+//        for (Folder folder: folders) {
+//            searchFolderWithSize(folder, size, foldersWithSize);
+//        }
+//        return foldersWithSize;
+//    }
 
     @Override
     public int count() {
@@ -125,7 +92,17 @@ public class FileCabinet implements Cabinet {
         return localCounter;
     }
 
-    private Folder searchFolderWithName(Folder folder, String name) {
+    private void countFolders(Folder folder) {
+        if (folder instanceof DefaultMultiFolder) {
+            DefaultMultiFolder searchFolder = (DefaultMultiFolder) folder;
+            for (Folder subFolder : searchFolder.getFolders()) {
+                this.counter++;
+                countFolders(subFolder) ;
+            }
+        }
+    }
+
+    private Folder searchFolderByNameWithRecursion(Folder folder, String name) {
         if (folder == null) {
             return null;
         }
@@ -139,7 +116,7 @@ public class FileCabinet implements Cabinet {
             }
 
             for (Folder subFolder : searchFolder.getFolders()) {
-                Folder returnedFromSubFolder = searchFolderWithName(subFolder, name);
+                Folder returnedFromSubFolder = searchFolderByNameWithRecursion(subFolder, name);
                 if (returnedFromSubFolder != null) {
                     return returnedFromSubFolder;
                 }
@@ -154,7 +131,7 @@ public class FileCabinet implements Cabinet {
         return null;
     }
 
-    private void searchFolderWithSize(Folder folder, String size, List<Folder> folders) {
+    private void searchFolderBySizeWithRecursion(Folder folder, String size, List<Folder> folders) {
         if (folder instanceof DefaultMultiFolder) {
 
             DefaultMultiFolder searchFolder = (DefaultMultiFolder) folder;
@@ -164,52 +141,42 @@ public class FileCabinet implements Cabinet {
             }
 
             for (Folder subFolder : searchFolder.getFolders()) {
-                System.out.println("Szukam w subFolder: " + subFolder);
-                searchFolderWithSize(subFolder, size, folders);
+                searchFolderBySizeWithRecursion(subFolder, size, folders);
             }
         }
 
         if (folder instanceof DefaultFolder) {
             if (folder.getSize().equals(size)) {
-                System.out.println("Dodaje folder: " + folder.getSize());
                 folders.add(folder);
             }
         }
     }
 
-    private void countFolders(Folder folder) {
-
-        System.out.println("CLASS TYPE: " + folder.getClass() + " || " +  folder.getName() + " || ");
-        if (folder instanceof DefaultMultiFolder) {
-
-            DefaultMultiFolder searchFolder = (DefaultMultiFolder) folder;
-            for (Folder subFolder : searchFolder.getFolders()) {
-                this.counter++;
-                countFolders(subFolder) ;
-            }
-        }
-    }
-
     private void generateData(int number, int maxNumberOfNestedFolder, int maxOfNest) {
+        int numberOfFolders = 0;
         for (int i = 0; i < number; i++) {
             if (getRandomBoolean()) {
                 Size size = getRandomBoolean() ? Size.LARGE : getRandomBoolean() ? Size.MEDIUM : Size.SMALL;
                 this.folders.add(new DefaultFolder("Random " + i, size));
+                numberOfFolders++;
             } else {
                 Size size = getRandomBoolean() ? Size.LARGE : getRandomBoolean() ? Size.MEDIUM : Size.SMALL;
                 DefaultMultiFolder multiFolder = new DefaultMultiFolder("Random " + i, size);
-
+                numberOfFolders++;
                 for (int j = 0; j < getRandomOfNestedFolders(maxNumberOfNestedFolder); j++) {
                     if (getRandomBoolean()) {
                         Size size1 = getRandomBoolean() ? Size.LARGE : getRandomBoolean() ? Size.MEDIUM : Size.SMALL;
                         multiFolder.getFolders().add(new DefaultFolder("Random " + i + " " + j, size));
+                        numberOfFolders++;
                     } else {
                         Size size1 = getRandomBoolean() ? Size.LARGE : getRandomBoolean() ? Size.MEDIUM : Size.SMALL;
                         DefaultMultiFolder multiFolder1 = new DefaultMultiFolder("Random " + i + " " + j, size1);
+                        numberOfFolders++;
 
                         for (int k = 0; k < getRandomOfNest(maxOfNest); k++) {
                             Size size2 = getRandomBoolean() ? Size.LARGE : getRandomBoolean() ? Size.MEDIUM : Size.SMALL;
                             multiFolder1.getFolders().add(new DefaultFolder("Random " + i + " " + j + " " + k, size));
+                            numberOfFolders++;
                         }
 
                         multiFolder.getFolders().add(multiFolder1);
@@ -219,6 +186,7 @@ public class FileCabinet implements Cabinet {
                 this.folders.add(multiFolder);
             }
         }
+        System.out.println("Number of Folders: " + numberOfFolders);
     }
 
     private boolean getRandomBoolean() {
